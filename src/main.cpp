47e584bd4808c74820db1152c1420ca4854aa028
Vsevolod <114590209+porunit@users.noreply.github.com>
2024-01-13 23:35:15 +0300

    transpose test

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <opencv2/opencv.hpp>
+#include "matrix.hpp"
+#include "Figure.hpp"
+
+int main() {
+    Figure deezFigure = Figure();
+
+    deezFigure.rotate();
+    deezFigure.rotate();
+    deezFigure.rotate();
+    deezFigure.rotate();
+}
