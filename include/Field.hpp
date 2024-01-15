b7a40cc9c6c03fe5a3dc2da5fe331a80235ee44f
Vsevolod <114590209+porunit@users.noreply.github.com>
2024-01-15 03:11:14 +0300

    bag fix< random figures added

--- include/Field.hpp
+++ include/Field.hpp
@@ -1,32 +1,64 @@
 #pragma once
 
 #include <vector>
+#include <map>
 #include "Figure.hpp"
+
+
+static const int DEFAULT_COORDINATES_VALUE = 0;
+static const int EMPTY_FIELD = 0;
+static const int FIELD_HEIGHT = 20;
+static const int FIELD_WIDTH = 10;
+
 
 //TODO ref
 class Field {
+    friend class GameManager;
+
 public:
-    std::vector<std::vector<int>> getField();
-    void showField();
+
+    static int getFieldHeight();
+
+    std::vector<std::vector<int>> *getField();
+
     void bindFigure(Figure *figure);
+
     void unbindFigure();
+
     void processStep();
+
     int removeFullLines();
+
     void clear();
+
     bool checkUnderFigure();
+
     void transformFigure();
+
     void skipFigure();
+
     void moveFigureX(int direction);
+
     Field();
+
 private:
     std::vector<std::vector<int>> field;
     int figureX, figureY;
     Figure *activeFigure;
+    std::map<int, Figure*> figurePresets;
+    int presetCounter;
+
+    void initFigurePresets();
     void setFigureTiles();
+
     void removeFigureTiles();
+
     void balanceField(int y);
 
+    bool checkRightOrLeftFigure(int dir);
+
     bool checkTransformFigure();
+    int getPresetCounter();
 };
 
-std::ostream& operator<<(std::ostream& os, Field& f);
+std::ostream &operator<<(std::ostream &os, Field &f);
